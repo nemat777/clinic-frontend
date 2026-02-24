738bfa534eaeb075c5c4d57ef1f447927df4e0e3
nemat777 <31022848+nemat777@users.noreply.github.com>
2026-02-23 18:02:24 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 // src/App.js
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import PatientIntakePage from './PatientIntakePage';
 import ProtectedDashboard from './ProtectedDashboard';
 
 function App() {
   return (
-    <div>
-      {/* Replace this officeId with dynamic value per office later */}
-      <ProtectedDashboard officeId="OFFICE1_UUID" />
-    </div>
+    <Router>
+      <Routes>
+        {/* Public patient intake */}
+        <Route path="/" element={<PatientIntakePage />} />
+
+        {/* Protected doctor dashboard */}
+        <Route path="/dashboard" element={<ProtectedDashboard officeId="OFFICE1_UUID" />} />
+      </Routes>
+    </Router>
   );
 }
 
